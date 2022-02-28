221f6ad257fd299abf1bd6f55134804df0a33c63
Farrukh <faraxunov@gmail.com>
2022-03-01 00:21:30 +0500

    second commit

--- bin/prototype.dart
+++ bin/prototype.dart
@@ -1,19 +1,13 @@
 import 'package:prototype/prototype.dart';
 
 void main(List<String> arguments) {
-  Map<String, dynamic> firstDriverData = {
-    'id': 1,
-    'name': 'Farrukh',
-    'vehicle': 'MB G63',
-    'tariff': 'premium'
-  };
   Map<String, dynamic> secondDriverData = {
     'id': 2,
     'name': 'Pavlo',
     'vehicle': 'BMW X6',
     'tariff': 'premium'
   };
-  DriverData firstDriver = DriverData(firstDriverData);
+  DriverData firstDriver = DriverData();
   print('---'*30);
   print('First driver:\n${firstDriver.getData()}');
   DriverData secondDriver = firstDriver.clone();
@@ -23,4 +17,6 @@ void main(List<String> arguments) {
   print('Hash code of first driver:\n${firstDriver.hashCode}\nHash code of second driver:\n${secondDriver.hashCode}');
   print('---'*30);
   print('Are first driver and second driver identical?: ${identical(firstDriver, secondDriver)}');
+  DriverData thirdDriver = secondDriver.clone();
+  print('Third driver:\n${thirdDriver.getData()}');
 }
--- lib/prototype.dart
+++ lib/prototype.dart
@@ -1,15 +1,30 @@
-class DriverData {
-  Map<String, dynamic> _data;
+abstract class IDriverData {
+  DriverData clone();
+}
 
-  DriverData(this._data);
-  void writeData(Map<String, dynamic> data) {
-    _data = data;
+class DriverData implements IDriverData {
+  Map<String, dynamic> data;
+  
+  DriverData({this.data = defaultData});
+  factory DriverData._(DriverData driverData) {
+    return DriverData(data: driverData.data);
+  }
+  void writeData(Map<String, dynamic> dataToWrite) {
+    data = dataToWrite;
   }
   Map<String, dynamic> getData() {
-    return _data;
+    return data;
   }
 
-  clone(){
-    return DriverData(_data);
+  @override
+  DriverData clone() {
+    return DriverData._(this);
   }
 }
+
+const Map<String, dynamic> defaultData = {
+    'id': 1,
+    'name': 'Farrukh',
+    'vehicle': 'MB G63',
+    'tariff': 'premium'
+  };
